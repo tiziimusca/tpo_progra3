476e47e349349a8ef1b4d1aa78a0b68b7233099a
nzeisel <163483902+nzeisel@users.noreply.github.com>
2024-11-05 18:09:46 -0300

    Add files via upload

---
+++ app/nina/CentroDeDistribucion.java
@@ -0,0 +1,15 @@
+public class CentroDeDistribucion {
+    int id;
+    double costoTransportePuerto;
+    double costoOperacionAnual;
+
+    public CentroDeDistribucion(int id, double costoTransportePuerto, double costoOperacionAnual) {
+        this.id = id;
+        this.costoTransportePuerto = costoTransportePuerto;
+        this.costoOperacionAnual = costoOperacionAnual;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+}
---
+++ app/nina/Dijkstra.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Dijkstra {
+    public static double[] dijkstra(int origen, int numNodos, List<List<Ruta>> grafo) {
+        double[] distancias = new double[numNodos];
+        Arrays.fill(distancias, Double.MAX_VALUE);
+        distancias[origen] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> a[1]));
+        pq.add(new int[]{origen, 0});
+
+        while (!pq.isEmpty()) {
+            int[] actual = pq.poll();
+            int nodo = actual[0];
+            double costo = actual[1];
+
+            if (costo > distancias[nodo]) continue;
+
+            for (Ruta ruta : grafo.get(nodo)) {
+                int vecino = ruta.destino;
+                double nuevoCosto = costo + ruta.costoTransporte;
+
+                if (nuevoCosto < distancias[vecino]) {
+                    distancias[vecino] = nuevoCosto;
+                    pq.add(new int[]{vecino, (int) nuevoCosto});
+                }
+            }
+        }
+
+        return distancias;
+    }
+}
